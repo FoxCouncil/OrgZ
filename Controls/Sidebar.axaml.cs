97433c5bef3db612dbb4b40b5eea90ec03d5b738
Fox <onefox@gmail.com>
2026-03-10 12:14:45 -0700

    Test

--- Controls/Sidebar.axaml.cs
+++ Controls/Sidebar.axaml.cs
@@ -1,13 +1,52 @@
 // Copyright (c) 2026 FoxCouncil (https://github.com/FoxCouncil/OrgZ)
 
 using Avalonia.Controls;
+using OrgZ.Models;
+using OrgZ.ViewModels;
 
 namespace OrgZ.Controls;
 
 public partial class Sidebar : UserControl
 {
+    private bool _suppressSelectionChange;
+
     public Sidebar()
     {
         InitializeComponent();
+    }
+
+    private void LibraryListBox_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (_suppressSelectionChange)
+        {
+            return;
+        }
+
+        if (LibraryListBox.SelectedItem != null)
+        {
+            _suppressSelectionChange = true;
+            PlaylistListBox.SelectedItem = null;
+            _suppressSelectionChange = false;
+        }
+    }
+
+    private void PlaylistListBox_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (_suppressSelectionChange)
+        {
+            return;
+        }
+
+        if (PlaylistListBox.SelectedItem is SidebarItem item)
+        {
+            _suppressSelectionChange = true;
+            LibraryListBox.SelectedItem = null;
+            _suppressSelectionChange = false;
+
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.SelectedSidebarItem = item;
+            }
+        }
     }
 }
